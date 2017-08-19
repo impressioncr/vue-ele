3c14dc53320663fc7ea69a3be5d1e8f8316d780c
impressioncr <impressioncr@hotmail.com>
2017-08-19 14:07:50 +0800

    ratings

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -34,9 +34,26 @@
         <div class="rating">
           <h1 class="title">商品评价</h1>
           <ratingselect
-            :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"
+            :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings" @select="selectChange" @toggle="toggleContent"
           >
           </ratingselect>
+          <div class="rating-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img :src="rating.avatar" class="avatar" width="12px" height="12px">
+                </div>
+                <div class="time">
+                  {{rating.rateTime | formatDate}}
+                </div>
+                <p class="text">
+                  <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                </p>
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+          </div>
         </div>
       </div>
     </div>
@@ -53,6 +70,7 @@
   import split from '../split/split'
   import ratingselect from '../ratingselect/ratingselect'
   import Vue from 'vue'
+  import {formatDate} from '../../common/js/date'
 
   export default {
     props:{
@@ -97,9 +115,37 @@
         this.$emit('add', event.target)
         Vue.set(this.food, 'count', 1)
       },
+      needShow(type, text) {
+        if(this.onlyContent && !text) {
+          return false
+        }
+        if(this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      },
       addFood(target) {
         this.$emit('add', target)
       },
+      selectChange(type) {
+        this.selectType = type
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      toggleContent() {
+        this.onlyContent = !this.onlyContent
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      }
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time)
+        return formatDate(date,'yyyy-MM-dd hh:mm')
+      }
     },
     components: {
       cart,
@@ -111,6 +157,7 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
+
   .food
     position: fixed
     left: 0
@@ -197,17 +244,63 @@
         &.fade-enter, &.fade-leave-active
           opacity 0
           z-index -1
+    title(margin, val)
+      line-height 14px
+      margin-{margin} val
+      font-size 14px
+      color rgb(7, 17, 27)
     .info
       padding 18px
       .title
-        line-height 17px
-        margin-bottom 6px
-        font-size 14px
-        color rgb(7, 17, 27)
+        title bottom 6px
       .text
         font-size 12px
         padding 0 8px
         line-height 2*@font-size
         color rgb(77, 85, 93)
-
+    .rating
+      padding-top 18px
+      .title
+        title left 18px
+      .rating-wrapper
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px rgba(7, 17, 27, 0.1)
+          .user
+            position absolute
+            right 0
+            top 16px
+            font-size 0
+            line-height 13px
+            .name
+              display inline-block
+              margin-right 6px
+              vertical-align top
+              font-size 10px
+              color rgb(147,153,159)
+            .avatar
+              border-radius 50%
+          .time
+            margin-bottom 6px
+            line-height 12px
+            font-size 10px
+            color rgb(147,153,159)
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7, 17, 27)
+            .icon-thumb_up, .icon-thumb_down
+              line-height 16px
+              margin-right 4px
+              font-size 12px
+            .icon-thumb_up
+              color rgb(0,160,220)
+            .icon-thumb_down
+              color rgb(147,153,159)
+        .no-rating
+          padding 16px 0
+          font-size 12px
+          color rgb(147, 153, 159)
 </style>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
-      <span>{{desc.all}}</span>
-      <span>{{desc.position}}</span>
-      <span>{{desc.negative}}</span>
+    <div class="rating-type border-1px">
+      <span @click="select(2, $event)" class="block positive" :class="{active: selectType===2}">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+      <span @click="select(0, $event)" class="block positive" :class="{active: selectType===0}">{{desc.position}}<span class="count">{{positives.length}}</span></span>
+      <span @click="select(1, $event)" class="block negative" :class="{active: selectType===1}">{{desc.negative}}<span class="count">{{negative.length}}</span></span>
     </div>
-    <div class="switch">
+    <div @click="toggleContent" class="switch" :class="{on: onlyContent}">
       <span class="icon-check_circle"></span>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -42,10 +42,79 @@
           }
         }
       }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE
+        })
+      },
+      negative() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE
+        });
+      }
+    },
+    methods: {
+      select(type, event) {
+        if(!event._constructed) {
+          return
+        }
+        this.$emit('select', type)
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return
+        }
+        this.$emit('toggle')
     }
   }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import "../../common/stylus/mixin"
+  .ratingselect
+    .rating-type
+      padding 18px 0
+      margin 0 18px
+      border-1px rgba(7, 17, 27, 0.1)
+      font-size 0
+      .block
+        display inline-block
+        padding 8px 12px
+        margin-right 8px
+        border-radius 1px
+        line-height 16px
+        color rgb(77, 85, 93)
+        font-size 12px
+        &.active
+          color #fff
+        .count
+          margin-left 2px
+          font-size 8px
+        &.positive
+          background rgba(0, 160, 220, 0.2)
+          &.active
+            background rgb(0, 160, 220)
+        &.negative
+          background rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
+    .switch
+      padding 12px 18px
+      line-height 24px
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+      color rgb(147, 153, 159)
+      font-size 0
+      &.on
+        .icon-check_circle
+          color #00c850
+      .icon-check_circle
+        display inline-block
+        vertical-align top
+        margin-right 4px
+        font-size 24px
+      .text
+        font-size 12px
 </style>
